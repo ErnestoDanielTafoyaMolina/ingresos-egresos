4c5b833fe09f5aeb3a3ef38befb6225c3e021f81
ErnestoDanielTafoyaMolina <edaniimol@gmail.com>
2024-05-06 01:39:18 -0600

    need the user test the app

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FinanzasService, RegistroFinanciero } from '../services/finanzas.service';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  fechaInicio: string = new Date().toISOString();
+  fechaFin: string = new Date().toISOString();
+  totalIngresos = 0;
+  totalEgresos = 0;
+  porcentajeEgresos = 0;
+  balanceMostrado = false;
 
-  constructor() {}
+  constructor(private finanzasService: FinanzasService) {}
 
+  calcularBalance() {
+    const inicio = new Date(this.fechaInicio);
+    inicio.setHours(0, 0, 0, 0); // Establecer a medianoche del día seleccionado
+    const fin = new Date(this.fechaFin);
+    fin.setHours(23, 59, 59, 999); // Establecer al final del día seleccionado
+
+    this.totalIngresos = 0;
+    this.totalEgresos = 0;
+
+    this.finanzasService.obtenerRegistros().forEach(registro => {
+      const fechaRegistro = new Date(registro.anio, registro.mes - 1, registro.dia);
+      if (fechaRegistro >= inicio && fechaRegistro <= fin) {
+        this.totalIngresos += registro.ingresos;
+        this.totalEgresos += registro.egresos;
+      }
+    });
+
+    if (this.totalIngresos === 0) {
+      this.porcentajeEgresos = 0;
+    } else {
+      this.porcentajeEgresos = (this.totalEgresos / this.totalIngresos) * 100;
+    }
+
+    this.balanceMostrado = true;
+  }
 }
